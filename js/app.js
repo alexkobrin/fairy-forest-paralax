40991ee6d40b4f787e178e012117054232958017
Aliaksei <storyday2012@gmail.com>
2023-07-19 21:50:57 +0200

    update

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
 window.addEventListener('scroll' , e => {
   document.body.style.cssText = `--scrollTop : ${this.scrollY}px`
 })
+gsap.registerPlugin(ScrollTriger , ScrollSmoother)
+.ScrollSmoother.create({
+  wrapper: '.wrapper',
+  content: '.container'
+})
