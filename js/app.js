707fa20b0eaeb229856b03eb0cbc05d44a4137b3
Aliaksei <storyday2012@gmail.com>
2023-07-21 09:04:11 +0200

    upd

--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
-window.addEventListener('scroll' , e => {
-  document.body.style.cssText = `--scrollTop : ${this.scrollY}px`
+window.addEventListener('scroll', (e) => {
+  document.body.style.cssText += `--scrollTop : ${this.scrollY}px`
 })
-gsap.registerPlugin(ScrollTriger , ScrollSmoother)
-.ScrollSmoother.create({
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+ 
+//  const wrapper = window.document.getElementById('wrapper')
+//  const content = window.document.getElementById('content')
+  
+
+ScrollSmoother.create({
   wrapper: '.wrapper',
-  content: '.container'
+  content: '.content',
 })
